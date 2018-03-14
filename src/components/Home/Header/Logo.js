955fcf914e21f8c014710fdc1d8fffe1ebe836b8
Guido van de Riet <guidovdriet@gmail.com>
2018-03-14 22:28:02 +0100

    fixed navigation on scroll

--- src/components/Home/Header/Logo.js
+++ src/components/Home/Header/Logo.js
@@ -3,34 +3,51 @@ import styled from "styled-components";
 
 const BrandWrapper = styled.div`
   & > * {
-      color: ${props => props.theme.colorMediumGray};
-      font-size: 1.5em;
-      border: 1px solid ${props => props.theme.colorMediumGray};
-      margin-bottom: 9px;
+    color: ${props => props.theme.colorMediumGray};
+    font-size: 1.5em;
+    border: 1px solid ${props => props.theme.colorMediumGray};
+    margin-bottom: 9px;
+  }
+  transition: all .3s cubic-bezier(.25,.75,.5,1.25);
+  &.active {
+      display: flex;
+      margin-top: -20px;
+      transform: translate3d(100px, 0, 0);
   }
 `;
 
 const BrandTop = styled.h1`
-    padding: 5px 0px 5px 10px;
-    width: 70px;
+  padding: 5px 0px 5px 10px;
+  width: 70px;
+  margin-right: 10px;
 `;
 
 const BrandMiddle = styled.h1`
-    padding: 5px 0px 5px 10px;
-    width: 74px;
+  padding: 5px 0px 5px 10px;
+  width: 74px;
+  margin-right: 10px;
 `;
 
 const BrandBottom = styled.h1`
-    padding: 5px 0px 5px 10px;
-    width: 55px;
+  padding: 5px 0px 5px 10px;
+  width: 55px;
 `;
 
-const Logo = () => (
-  <BrandWrapper>
-    <BrandTop>Guido</BrandTop>
-    <BrandMiddle>van de</BrandMiddle>
-    <BrandBottom>Riet</BrandBottom>
-  </BrandWrapper>
-);
+class Logo extends Component {
+  componentDidMount() {
+      this.props.addNavBackgroundOnScroll(null, this.logo);
+  }
+
+  render() {
+    const { navScrollLimit } = this.props;
+    return (
+      <BrandWrapper innerRef={brand => (this.logo = brand)} className={navScrollLimit && 'active'}>
+        <BrandTop className={navScrollLimit && 'active'} >Guido</BrandTop>
+        <BrandMiddle className={navScrollLimit && 'active'}>van de</BrandMiddle>
+        <BrandBottom>Riet</BrandBottom>
+      </BrandWrapper>
+    );
+  }
+}
 
 export default Logo;
--- src/components/Home/Header/Navigation.js
+++ src/components/Home/Header/Navigation.js
@@ -2,25 +2,39 @@ import React, { Component } from 'react';
 import styled from 'styled-components';
 
 const Nav = styled.nav`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 40px 80px 0 80px;    
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 30px 30px 0 30px;
+  transition: position .3s ease-in-out;
+  &.active {
+    background: rgba(0, 0, 0, 0.9);
+    top: 0;
+    width: 100%;
+    height: 50px;
+    position: fixed;
+  }
+`;
 
 const SocialIconWrapper = styled.ul`
-    list-style: none;
-    display: flex;
-    width: 115px;
-    align-items: center;
-    justify-content: space-between;
-`
+  list-style: none;
+  display: flex;
+  width: 115px;
+  margin-bottom: 6px;
+  align-items: center;
+  justify-content: space-between;
+  transition: all .3s cubic-bezier(.25,.75,.5,1.25);
+  &.active {
+    transform: translate3d(-100px, 0, 0);
+    width: 135px;
+  }
+`;
 
 const Icon = styled.li`
-    > * {
-        fill: ${props => props.theme.colorMediumGray};
-    }
-`
+  > * {
+    fill: ${props => props.theme.colorMediumGray};
+  }
+`;
 
 // Icons
 import Logo from './Logo';
@@ -30,27 +44,56 @@ import LinkedIn from '../../images/icons/social-icons/LinkedIn';
 import Twitter from '../../images/icons/social-icons/Twitter';
 
 class Navigation extends Component {
-    render() {
-        return (
-            <Nav>
-                <Logo />
-                <SocialIconWrapper>
-                    <Icon>
-                        <Github />
-                    </Icon>
-                    <Icon>
-                        <Codepen />
-                    </Icon>
-                    <Icon>
-                        <Twitter />
-                    </Icon>
-                    <Icon>
-                        <LinkedIn />
-                    </Icon>
-                </SocialIconWrapper>
-            </Nav>
-        )
-    }
+  state = {
+    logoPositionBottom: 0,
+    navScrollLimit: false
+  };
+
+  addNavBackgroundOnScroll = (e, logo) => {
+    const scrollTop = window.pageYOffset;
+    logo &&
+      this.setState({
+        logoPositionBottom: logo.getBoundingClientRect().bottom
+      });
+    this.state.logoPositionBottom < scrollTop &&
+      this.setState({ navScrollLimit: true });
+    this.state.logoPositionBottom > scrollTop &&
+      this.setState({ navScrollLimit: false });
+  };
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.addNavBackgroundOnScroll);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('scroll', this.addNavBackgroundOnScroll);
+  }
+
+  render() {
+    const { navScrollLimit } = this.state;
+    return (
+      <Nav className={navScrollLimit && 'active'}>
+        <Logo
+          addNavBackgroundOnScroll={this.addNavBackgroundOnScroll}
+          navScrollLimit={navScrollLimit}
+        />
+        <SocialIconWrapper className={navScrollLimit && 'active'}>
+          <Icon>
+            <Github />
+          </Icon>
+          <Icon>
+            <Codepen />
+          </Icon>
+          <Icon>
+            <Twitter />
+          </Icon>
+          <Icon>
+            <LinkedIn />
+          </Icon>
+        </SocialIconWrapper>
+      </Nav>
+    );
+  }
 }
 
 export default Navigation;
